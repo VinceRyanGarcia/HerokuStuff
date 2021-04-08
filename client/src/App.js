2c0db3174f65c1a1a1c58befc90c9e28b30f0cd4
Vince Garcia <vinceryangarcia@gmail.com>
2021-04-08 10:54:20 -0700

    lol

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 import {useState, useEffect} from 'react'
 
 function url(path){
-  return process.env.NODE_ENV === "devevlopment"
+  return process.env.NODE_ENV === "development" 
   ? `http://localhost:1234${path}` : path
 }
 
 function App() {
   const [data, setData] = useState("Hi")
   useEffect(()=>{
-    setData(data+"!")
+    fetch(url("/api/"))
+    .then(res=>res.json())
+    .then(apiData => setData(apiData.data))
   },[])
-
   return (
     <div className="App">
       <header className="App-header">
